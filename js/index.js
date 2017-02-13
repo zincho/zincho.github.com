fcd7f0e411d62c1f18bb71e6287beeccf2e403b2
zincho <zincho@naver.com>
2017-02-13 17:50:40 +0900

    aa

--- js/index.js
+++ js/index.js
@@ -8,6 +8,8 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+var disqus_config;
+
 var Helper = function () {
     function Helper(props) {
         _classCallCheck(this, Helper);
@@ -101,6 +103,32 @@ var VideoPannel = function (_React$Component2) {
     }
 
     _createClass(VideoPannel, [{
+        key: "componentDidUpdate",
+        value: function componentDidUpdate() {
+            var me = this;
+
+            if (window.DISQUS) {
+                DISQUS.reset({
+                    reload: true,
+                    config: function config() {
+                        this.page.identifier = me.props.src;
+                        this.page.url = "https://zincho.github.io/index.html#!" + me.props.src;
+                    }
+                });
+            } else {
+                disqus_config = function disqus_config() {
+                    this.page.url = "https://zincho.github.io/index.html#!" + me.props.src;
+                    this.page.identifier = me.props.src;
+                };
+
+                var d = document,
+                    s = d.createElement('script');
+                s.src = '//zincho.disqus.com/embed.js';
+                s.setAttribute('data-timestamp', +new Date());
+                (d.head || d.body).appendChild(s);
+            }
+        }
+    }, {
         key: "handleClick",
         value: function handleClick() {
             if (helper.getState("openPanel") === this) return;
@@ -116,7 +144,8 @@ var VideoPannel = function (_React$Component2) {
             return React.createElement(
                 "div",
                 { className: "panel-body" },
-                React.createElement(Video, { src: this.props.src })
+                React.createElement(Video, { src: this.props.src }),
+                React.createElement("div", { id: "disqus_thread" })
             );
         }
     }, {
